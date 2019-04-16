dc5cefe7c29aa551c5a76ebaadb1d3b15d664721
shuyangli94 <shuyangli94@users.noreply.github.com>
2019-04-15 20:10:25 -0700

    columns

--- recipe_tagger/utils.js
+++ recipe_tagger/utils.js
@@ -1,8 +1,36 @@
 // GLOBAL VARIABLES
-var startColor = "#00FF00";
-var endColor = "#FF0000";
 var canvas = document.getElementById("dagCanvas");
 var context = canvas.getContext("2d");
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function randInt(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+var RECIPE = {
+	'name': '1956   stuffed onions',
+	'ingredients': ['onions','boiling water','salt','sausage','navy beans','pepper','dry mustard','breadcrumbs','celery tops','brown sugar'],
+	'steps': [
+		'cook onions in the boiling, salted water for 20 minutes; drain and cool.',
+		'cook sausage in frying pan until lightly browned; drain off fat.',
+		'remove the centers of the onions, leaving a bottom so that the filling does not fall through; set aside.',
+		'add beans, chopped onion centers, and seasonings to cooked sausage; mix well.',
+		'divide mixture and stuff into the 6 onion"bowls" you have created; place in baking dish.',
+		'mix bread crumbs, celery, and brown sugar together; sprinkle onto the top of each onion.',
+		'bake onions at 350 degrees f for 30 minutes.',
+		'wonderful when served with fresh, steamed green beans and clover leaf rolls.'
+	]
+};
+
+
+// GLOBAL VARIABLES
+var startColor = "#00FF00";
+var endColor = "#FF0000";
 var scaling = 8;
 var delay;
 var startY;
@@ -14,14 +42,6 @@ var playerX;
 var playerY;
 var stepsTaken;
 var ready = false;
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function randInt(min, max) {
-	return Math.floor(Math.random() * (max - min) + min);
-}
 
 function setDimensionConstraints(min_rows = minr, max_rows = maxr) {
 	minr = min_rows;
